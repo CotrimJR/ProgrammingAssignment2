298aa8d542eaa6e7c8ed7b7e5402ca5c9ebe8da5
CotrimJR <jose.cotrim@outlook.com>
2017-03-07 16:12:01 -0300

    Programming Assignment 2 - Coding Exercise

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## =====================================================
+## Caching the Inverse of a Matrix
+## =====================================================
+## Functions:
+##   makeCacheMatrix()  --> Create Cache Matrix
+##   cacheSolve()       --> Compute Inverse Matrix
+## Example of use:
+##   mtest <- makeCacheMatrix(matrix(1:4,nrow=2,ncol=2))
+##   cacheSolve(mtest)
+## =====================================================
 
-## Write a short comment describing this function
-
+## -----------------------------------------------------
+## This function creates a special "matrix" object that
+## can cache its inverse.
+## -----------------------------------------------------
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get,
+       setsolve = setsolve,
+       getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
-
+## -----------------------------------------------------
+## This function computes the inverse of the special
+## "matrix" returned by 'makeCacheMatrix'.
+## -----------------------------------------------------
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getsolve()
+  if(!is.null(m)) {
+    message("Getting cached data")
+    return(m)
+  }
+  data <- x$get()
+
+  if ( det(data) == 0 ) {
+    message("The matrix is singular, not invertible.")
+  } else {
+    m <- solve(data, ...)
+  }
+  x$setsolve(m)
+  m   ## Return a matrix that is the inverse of 'x'
 }
